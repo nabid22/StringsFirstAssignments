79b127a00dbd03e3d1df7147fc9a3cd85b7b3a5f
Asif Morshad Nabid <asifmorshadnabid@gmail.com>
2017-09-24 23:02:29 +0600

    this portion of code check whether the dna strand is uppercase it returns the upper case gene and when dna is in lowercase it return lowercase gene

---
+++ src/Part2.java
@@ -0,0 +1,52 @@
+
+public class Part2 {
+	
+	    public String findSimpleGene(String dna){
+	        int startIndex = dna.indexOf("ATG");
+	        int stopIndex = dna.indexOf("TAA", startIndex + 3);
+	        while(stopIndex != -1){
+	            if((stopIndex - startIndex) % 3 == 0){
+	                return dna.substring(startIndex, stopIndex + 3);
+	            }
+	            else{
+	                stopIndex = dna.indexOf("TAA", stopIndex + 1);
+	            }
+	        }
+	        return "";
+	    }
+	    void testSimpleGene(){
+	        String dna = "ATGGGTTAAGTC";
+	        char ch = dna.charAt(0);
+	        String startCodon = "", stopCodon = "";
+	        if(Character.isUpperCase(ch)){
+	            startCodon = "ATG";
+	            stopCodon = "TAA";
+	        }
+	        else{
+	            startCodon = "atg";
+	            stopCodon = "taa";
+	        }
+	        
+	        System.out.println("DNA strand is " + dna);
+	        String gene = findSimpleGene(dna);
+	        System.out.println("Gene is " + gene);
+	        
+	        dna = "gatgctataat";
+	        ch = dna.charAt(0);
+	        if(Character.isUpperCase(ch)){
+	            startCodon = "ATG";
+	            stopCodon = "TAA";
+	        }
+	        else{
+	            startCodon = "atg";
+	            stopCodon = "taa";
+	        }
+	        System.out.println("DNA strand is " + dna);
+	        gene = findSimpleGene(dna);
+	        System.out.println("Gene is " + gene);
+	        /*dna = "CGATGGTTGATAAGCCTAAGCTAAA";
+	        System.out.println("DNA strand is " + dna);
+	        gene = findSimpleGene(dna, startCodon, stopCodon);
+	        System.out.println("Gene is " + gene);*/
+	    }
+}
