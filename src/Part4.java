6bdd698fb129b8f3ab854038f59f1cfadeeeb489
Asif Morshad Nabid <asifmorshadnabid@gmail.com>
2017-09-25 00:55:08 +0600

    method checkYoytube is able to check backslash

--- src/Part4.java
+++ src/Part4.java
@@ -1,4 +1,18 @@
-
+import edu.duke.*;
 public class Part4 {
+	public void checkYoytube(){
+		URLResource res = new URLResource("http://www.dukelearntoprogram.com/course2/data/manylinks.html"); 
+		for(String word : res.words()){
+			int x = word.indexOf("youtube.com");
+			int l = word.length();
+			if(x != -1){
+				int startIndex = word.indexOf("\"");
+				int endIndex = word.lastIndexOf("\"", l);
 
+			}
+		}
+	}
 }
+
+
+
