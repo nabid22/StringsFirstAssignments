49ea993ae88dbb72fd0db9288684afebb87987ba
Asif Morshad Nabid <asifmorshadnabid@gmail.com>
2017-09-25 00:12:07 +0600

    This method should call twoOccurrences on several pairs of strings and print the strings and the result of calling twoOccurrences (true or false) for each pair. Be sure to test examples that should result in true and examples that should result in false.

--- src/Part3.java
+++ src/Part3.java
@@ -1,4 +1,53 @@
 
 public class Part3 {
+	public boolean twoOccurrences(String stringa, String stringb){
+		int curIndex = 0, count = 0;
+		int l = stringa.length();
+		curIndex = stringb.indexOf(stringa, curIndex);
+		while(true){
+			if(curIndex != -1){
+				count++;
+				if(count == 2){
+					return true;
+				}
+			}
+			else{
+				return false;
+			}
+			curIndex = stringb.indexOf(stringa, curIndex + l);
+		}
+	}
 
+public void testing(){
+    String stringa = "by";
+    String stringb = "A story by Abby Long";
+    System.out.println(stringa);
+    System.out.println(stringb);
+    if(twoOccurrences(stringa, stringb)){
+        System.out.println(twoOccurrences(stringa, stringb));
+    }
+    else{
+        System.out.println(twoOccurrences(stringa, stringb));
+    }
+    stringa = "atg";
+    stringb = "ctgtatgta";
+    System.out.println(stringa);
+    System.out.println(stringb);
+    if(twoOccurrences(stringa, stringb)){
+        System.out.println(twoOccurrences(stringa, stringb));
+    }
+    else{
+        System.out.println(twoOccurrences(stringa, stringb));
+    }
+    stringa = "an";
+    stringb = "banana";
+    System.out.println(stringa);
+    System.out.println(stringb);
+   // System.out.println(lastPart(stringa, stringb));
+    stringa = "zoo";
+    stringb = "forest";
+    System.out.println(stringa);
+    System.out.println(stringb);
+   // System.out.println(lastPart(stringa, stringb));
+}
 }
